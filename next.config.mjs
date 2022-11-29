2553f7f881a644ed60d338a60bc1cb134101b544
gabriel-ocp <102222409+gabriel-ocp@users.noreply.github.com>
2022-11-29 11:18:21 +0000

    Use es modules

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+import nextra from 'nextra';
+import remarkGfm from 'remark-gfm';
+
+//import remarkMath from 'remark-math';
+//import rehypeKatex from 'rehype-katex';
+//const remarkDefinitionList = require('remark-deflist');
+
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.js',
+  unstable_staticImage: true,
+  // ...
+  mdxOptions: {
+    remarkPlugins: [ /*remarkMath,*/ remarkGfm ],
+    rehypePlugins: [ /*rehypeKatex*/ ],   
+  },
+});
+
+//module.exports = withNextra();
+export default withNextra();
